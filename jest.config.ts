5e41cb4f4141600a72d574618675e0c6f9d5f971
Douglas Soares <douglasmaismcpe@gmail.com>
2021-11-24 10:28:36 -0300

    refactored configurations

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,4 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
+import { pathsToModuleNameMapper } from "ts-jest/utils";
 
 export default {
   // All imported modules in your tests should be mocked automatically
@@ -81,7 +78,9 @@ export default {
   // ],
 
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
-  // moduleNameMapper: {},
+  moduleNameMapper: pathsToModuleNameMapper({
+    prefix: ["<rootDir>/src/"],
+  }),
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -146,9 +145,7 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: [
-    "/.spec.ts"
-  ],
+  testMatch: ["**/*.spec.ts"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
